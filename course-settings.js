32fcc631acdd73c02f6b7ddae33f5fc008061b08
Jori Lampi <jori.lampi@helsinki.fi>
2022-12-02 15:33:31 +0200

    Update course configuration

--- course-settings.js
+++ course-settings.js
@@ -1,76 +1,76 @@
 const courseSettings = {
   language: "fi",
-  name: "Ohjelmoinnin perusteet ja jatkokurssi 2022",
-  siteUrl: "https://ohjelmointi-22.mooc.fi",
-  githubUrl: "https://github.com/rage/ohjelmointi-22",
+  name: "Ohjelmoinnin perusteet ja jatkokurssi 2023",
+  siteUrl: "https://ohjelmointi-23.mooc.fi",
+  githubUrl: "https://github.com/rage/ohjelmointi-23",
   subtitle: "Johdatus ohjelmointiin Python-kielellä",
-  slug: "ohjelmointi-2022",
+  slug: "ohjelmointi-2023",
   organizationName: "MOOC",
-  tmcCourse: "ohjelmointi-2022",
+  tmcCourse: "ohjelmointi-2023",
   quizzesId: "25385a90-234b-45e5-9fc2-1863d619543c",
   tmcOrganization: "mooc",
   bannerPath: "banner.svg",
   showExerciseDescriptionWhenNotLoggedIn: true,
   courseVariants: [
-    {
-      tmcOrganization: "hbc",
-      tmcCourse: "hbc-python-TVT22A",
-      // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
-    },
-    {
-      tmcOrganization: "hbc",
-      tmcCourse: "hbc-python-TVT22B",
-      // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
-    },
-    {
-      tmcOrganization: "hbc",
-      tmcCourse: "hbc-python-TVT22C",
-      // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
-    },
-    {
-      tmcOrganization: "hbc",
-      tmcCourse: "hbc-python-TVT22D",
-      // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
-    },
-    {
-      tmcOrganization: "hbc",
-      tmcCourse: "hbc-python-TVT22E",
-      // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
-    },
-    {
-      tmcOrganization: "hbc",
-      tmcCourse: "hbc-python-TVT22E-Group",
-      // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
-    },
-    {
-      tmcOrganization: "hbc",
-      tmcCourse: "hbc-python-TVT22F",
-      // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
-    },
-    {
-      tmcOrganization: "haaga-helia",
-      tmcCourse: "python-2022",
-    },
-    {
-      tmcOrganization: "keravanlukio",
-      tmcCourse: "tkt01-python-2021-22",
-    },
-    {
-      tmcOrganization: "hellu",
-      tmcCourse: "python-2022",
-    },
-    {
-      tmcOrganization: "graninlukio",
-      tmcCourse: "tvt4-python-2022",
-    },
-    {
-      tmcOrganization: "vuolu",
-      tmcCourse: "Vuolun-python-mooc-2022",
-    },
-    {
-      tmcOrganization: "uwasa",
-      tmcCourse: "python-2022",
-    }
+    // {
+    //   tmcOrganization: "hbc",
+    //   tmcCourse: "hbc-python-TVT22A",
+    //   // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
+    // },
+    // {
+    //   tmcOrganization: "hbc",
+    //   tmcCourse: "hbc-python-TVT22B",
+    //   // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
+    // },
+    // {
+    //   tmcOrganization: "hbc",
+    //   tmcCourse: "hbc-python-TVT22C",
+    //   // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
+    // },
+    // {
+    //   tmcOrganization: "hbc",
+    //   tmcCourse: "hbc-python-TVT22D",
+    //   // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
+    // },
+    // {
+    //   tmcOrganization: "hbc",
+    //   tmcCourse: "hbc-python-TVT22E",
+    //   // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
+    // },
+    // {
+    //   tmcOrganization: "hbc",
+    //   tmcCourse: "hbc-python-TVT22E-Group",
+    //   // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
+    // },
+    // {
+    //   tmcOrganization: "hbc",
+    //   tmcCourse: "hbc-python-TVT22F",
+    //   // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
+    // },
+    // {
+    //   tmcOrganization: "haaga-helia",
+    //   tmcCourse: "python-2022",
+    // },
+    // {
+    //   tmcOrganization: "keravanlukio",
+    //   tmcCourse: "tkt01-python-2021-22",
+    // },
+    // {
+    //   tmcOrganization: "hellu",
+    //   tmcCourse: "python-2022",
+    // },
+    // {
+    //   tmcOrganization: "graninlukio",
+    //   tmcCourse: "tvt4-python-2022",
+    // },
+    // {
+    //   tmcOrganization: "vuolu",
+    //   tmcCourse: "Vuolun-python-mooc-2022",
+    // },
+    // {
+    //   tmcOrganization: "uwasa",
+    //   tmcCourse: "python-2022",
+    // }
   ],
 }
 
